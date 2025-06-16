536cf375e5d58236310f4d3b6437d51e2a0910ac
Andrew Waters <waters@me.com>
2025-06-16 22:39:36 +0100

    Initial Commit

---
+++ Container Desktop/Container_DesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Container_DesktopApp.swift
+//  Container Desktop
+//
+//  Created by Andrew Waters on 16/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Container_DesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Container DesktopTests/Container_DesktopTests.swift
@@ -0,0 +1,16 @@
+//
+//  Container_DesktopTests.swift
+//  Container DesktopTests
+//
+//  Created by Andrew Waters on 16/06/2025.
+//
+
+import Testing
+
+struct Container_DesktopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
